a9a630dacc0a92a46b0109df55caa027f24d2f84
NachoMolinas <63751199+NachoMolinas@users.noreply.github.com>
2022-01-13 20:27:05 -0300

    Create ejercicios.py
    Resolución del cuadrado de un binomio

---
+++ ejercicios.py
@@ -0,0 +1,6 @@
+print("Cuadrado de un binomio \n")
+a = int(input("Ingrese un valor: "))
+b = int(input("Ingrese otro valor"))
+t1 = (a + b) * (a + b)
+t2 = a * a + (2 * a) * b + b * b
+print(t1,'Es igual a :', t2) 
