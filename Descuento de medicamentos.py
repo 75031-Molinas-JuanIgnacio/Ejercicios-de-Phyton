e85a7b256d72f025611355d23c8704c985b9ae6d
NachoMolinas <63751199+NachoMolinas@users.noreply.github.com>
2022-01-13 22:41:02 -0300

    Create Descuento de medicamentos.py
    Calcular el porcentaje del precio de un medicamento cualquiera

---
+++ Descuento de medicamentos.py
@@ -0,0 +1,6 @@
+
+print(" Medicamentos ")
+med = int(input("Ingrese el precio del medicamento"))
+desc =  (35 * med) //100
+monto_final = med - desc 
+print("El precio actual del Paracetamol ", med, " $ ", " El monto del descuento es de ", desc, " $ ", "Y el monto final a pagar es de ", monto_final, " $ " )
