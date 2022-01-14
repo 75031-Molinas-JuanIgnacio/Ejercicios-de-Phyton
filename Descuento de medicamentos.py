8a8bcf78952358b9b0f8f30e59d9d1198a7fa3d0
NicoBellanich <42323292+NicoBellanich@users.noreply.github.com>
2022-01-13 23:13:50 -0300

    Add cleancode
    Siempre pero siempre aprovechá la expresividad del lenguaje. Nada de acronimos. Hay que pensar bien el nombre de variables.
    Cosa que si viene alguien que no sabe nada a ver tu código se entienda.
    Por ejemplo dejar "35" en la linea que dice el descuento, no le esta diciendo nada a la persona que no sabe como se calcula el promedio. Si reemplazas por una variable que un humano entienda el codigo es mucho mas mantenible.
    Por otro lado el print(f'') es mas prolijo que el anterior! eso tambien cuenta !

--- Descuento de medicamentos.py
+++ Descuento de medicamentos.py
@@ -1,6 +1,9 @@
 
+
 print(" Medicamentos ")
-med = int(input("Ingrese el precio del medicamento"))
-desc =  (35 * med) //100
-monto_final = med - desc 
-print("El precio actual del Paracetamol ", med, " $ ", " El monto del descuento es de ", desc, " $ ", "Y el monto final a pagar es de ", monto_final, " $ " )
+medicamento =  "Paracetamol"
+precio_medicamento = int(input(f"Ingrese el precio del {medicamento} "))
+valor_descuento = 35
+descuento_final =  (valor_descuento * precio_medicamento) // 100
+monto_final = precio_medicamento - descuento_final 
+print(f'El precio actual del {medicamento} es $ {precio_medicamento}. El monto de descuento es $ {descuento_final} . Y el monto final a pagar es $ {monto_final}')
